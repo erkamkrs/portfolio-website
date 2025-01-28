cc8ecd55999b76083372bcf3c9956e0212993568
Erkam <erkamkiris@gmail.com>
2025-01-28 15:50:08 +0100

    ref(page): remove unused imports for cleaner code

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image"
-import Link from "next/link"
-import DeveloperImg from "@/public/developer.jpg"
-import { Button } from "@/components/ui/button"
 import Services from "@/components/developer/services"
 import Technologies from "@/components/developer/technologies"
 import Experience from "@/components/developer/experience"
 import Contact from "@/components/contact"
-import { FadeIn } from "@/components/ui/fadeIn"
 import Hero from "@/components/developer/hero"
 
 export default function DeveloperPage() {
